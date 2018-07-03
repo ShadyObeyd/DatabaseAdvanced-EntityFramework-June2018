b9b8abf7461cf4b6fcb00a470a623b18d781b2b5
ShadyObeyd <obeyd.shady@gmail.com>
2018-07-03 16:01:38 +0300

    Added 02.ORMFundamentals-Exercise

---
+++ 02.ORMFundamentals-Exercise/MiniORM.App/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace MiniORM.App
+{
+    using MiniORM.App.Data;
+    using MiniORM.App.Data.Entities;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string connectionString = @"Server=LAPTOP-SHADY\SQLEXPRESS;Database=MiniORM; Integrated Security=true";
+
+            SoftUniDbContext context = new SoftUniDbContext(connectionString);
+
+            context.Employees.Add(new Employee
+            {
+                FirstName = "Gosho",
+                LastName = "Inserted",
+                DepartmentId = context.Departments.First().Id,
+                IsEmployed = true
+            });
+
+            Employee employee = context.Employees.Last();
+            employee.FirstName = "Modified";
+
+            context.SaveChanges();
+        }
+    }
+}
