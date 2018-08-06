83171ba25842ad0c96d2530c9340f6bff5bc9e75
ShadyObeyd <obeyd.shady@gmail.com>
2018-08-06 19:18:58 +0300

    Added 12.Workshop

---
+++ 12.Workshop/TeamBuilder.Data/ConnectionConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace TeamBuilder.Data
+{
+    public class ConnectionConfiguration
+    {
+        public const string ConnectionString = @"Server=LAPTOP-SHADY\SQLEXPRESS;Database=TeamBuilder;Integrated Security=True";
+    }
+}
